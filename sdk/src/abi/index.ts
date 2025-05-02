1c9f4b98976c5f55f3e3b6ee4daccfe4ba0078eb
VitoGallacher <ccabdinedn@rambler.ru>
2025-05-02 11:23:49 +0800

    feat: add SDK ABI exports

---
+++ sdk/src/abi/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Contract ABI exports
+ */
+
+export { default as LivenessGatewayABI } from './LivenessGateway.json';
+export { default as AttestationRegistryABI } from './AttestationRegistry.json';
+export { default as NullifierSetABI } from './NullifierSet.json';
+export { default as ScoreEngineABI } from './ScoreEngine.json';
+export { default as ZKVerifierABI } from './ZKVerifier.json';
+export { default as PenaltyModuleABI } from './PenaltyModule.json';
+
